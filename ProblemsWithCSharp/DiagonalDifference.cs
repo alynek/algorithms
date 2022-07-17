7c94151372d078fcc2841a673426c18339e457ee
alynek <alyne0x67@gmail.com>
2022-07-17 12:01:54 -0300

    Create C# project for diagonal difference problem

---
+++ ProblemsWithCSharp/DiagonalDifference.cs
@@ -0,0 +1,70 @@
+﻿/*
+Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+For example, the square matrix  is shown below:
+
+1 2 3
+4 5 6
+9 8 9  
+The left-to-right diagonal = . The right to left diagonal = . Their absolute difference is .
+
+Function description
+
+Complete the  function in the editor below.
+
+diagonalDifference takes the following parameter:
+
+int arr[n][m]: an array of integers
+Return
+
+int: the absolute diagonal difference
+Input Format
+
+The first line contains a single integer, , the number of rows and columns in the square matrix .
+Each of the next  lines describes a row, , and consists of  space-separated integers .
+
+Constraints
+
+Output Format
+
+Return the absolute difference between the sums of the matrix's two diagonals as a single integer.
+ */
+
+namespace ProblemsWithCSharp
+{
+    public class DiagonalDifference
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<List<int>> arr = new List<List<int>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+            }
+
+            int result = diagonalDifference(arr);
+
+            Console.WriteLine(result); ;
+		}
+        public static int diagonalDifference(List<List<int>> arr)
+        {
+            int sumPrimaryDiagonal = 0;
+            int sumSecondaryDiagonal = 0;
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                for(int p = i; p <= i; p++)
+                {
+                    sumPrimaryDiagonal += arr[i][p];
+
+                    var s = (arr.Count-1) - p;
+                    sumSecondaryDiagonal += arr[i][s];
+                }
+            }
+            return Math.Abs(sumPrimaryDiagonal - sumSecondaryDiagonal);
+        }
+    }
+}
